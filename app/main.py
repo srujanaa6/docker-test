163d8badf3291e4855fd26dd6abe12049060e99f
Srujana Prabhu <108930902+srujanaa6@users.noreply.github.com>
2025-02-04 12:58:01 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/health', methods=['GET'])
 def health_check():
-    print("hi")
+    print("bye")
     return jsonify({"status": "ok"}), 200
 
 if __name__ == "__main__":
